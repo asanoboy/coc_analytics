991b95a39ea190e524caa26604241f6488ab04c7
asano <y.a.personal@gmail.com>
2014-02-20 20:35:18 +0900

    Initialized graph condition.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
 initSvg();
 initSlide();
-initGesture();
+initGraph();
+drawGraph();
 
-var svg, slide, windowWidth;
+var svg, slide, windowWidth, condition = {};
 
 function initSvg(){
     var w = window,
@@ -81,7 +82,7 @@ function initSvg(){
 
 function initSlide(){
     var leftDest = svg.node().getBoundingClientRect().left,
-        leftOrig = windowWidth - 100;
+        leftOrig = windowWidth - 10;
     slide = d3.select('#slide-container')
         .style('display', 'block')
         .style('width', svg.node().clientWidth + 'px')
@@ -89,9 +90,134 @@ function initSlide(){
         .style('top', svg.node().getBoundingClientRect().top + 'px')
         .style('left', leftOrig + 'px')
     ;
+
+    var lastPageX = false;
+    d3.select('#container')
+        .on('touchmove', function(){
+            if( lastPageX !== false ){
+                slide.style(
+                    'left',
+                    (parseInt(slide.style('left'), 10) +
+                    ( d3.event.touches[0].pageX - lastPageX)) + 'px'
+                );
+            }
+            lastPageX = d3.event.touches[0].pageX;
+        })
+        .on('touchend', function(){
+            lastPageX = false;
+            if( parseInt(slide.style('left'), 10) > windowWidth / 2 ){
+                slide.transition()
+                    .style('left', leftOrig + 'px');
+            }
+            else {
+                slide.transition()
+                    .style('left', leftDest + 'px');
+            }
+        });
+
+}
+
+function convertString(string){
+    switch(string){
+        case 'SPACE': return 'Space';
+        case 'TIME': return 'Time';
+        case 'DAMAGE': return 'Damage';
+        case 'HP': return 'Hitpoints';
+        case 'COST': return 'Cost';
+        default:
+            return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+}
+
+function initGraph(){
+    var columns = Object.keys(data[0].base).concat(Object.keys(data[0].level[0]));
+    [
+        ['#x-value1', 'HP'],
+        ['#x-value2', 'COST'],
+        ['#y-value1', 'DAMAGE'],
+        ['#y-value2', 'COST'],
+        ['#r-value1', 'COST'],
+        ['#r-value2', ''],
+    ]
+    .forEach(function(ar){
+        slide.select(ar[0])
+            .on('change', onChangeCondition)
+            .selectAll('option')
+            .data(columns)
+            .enter()
+            .append('option')
+            .attr('value', function(d){ return d; })
+            .each(function(d){
+                if( d === ar[1] ){
+                    d3.select(this).attr('selected', '');
+                }
+            })
+            .html(function(d){ return convertString(d); })
+        ;
+    });
+
+    var operators = [
+        ['', ''],
+        ['add', '+'],
+        ['div', '/'],
+        ['mul', 'x'],
+    ];
+    [
+        ['#x-operator', 'div',  '#x-value2'],
+        ['#y-operator', 'div',  '#y-value2'],
+        ['#r-operator', '',     '#r-value2'],
+    ]
+    .forEach(function(ar){
+        slide.select(ar[0])
+            .on('change', onChangeCondition)
+            .selectAll('option')
+            .data(operators)
+            .enter()
+            .append('option')
+            .attr('value', function(d){ return d[0]; })
+            .each(function(d){
+                if( d[0] === ar[1] ){
+                    d3.select(this).attr('selected', '');
+                }
+            })
+            .html(function(d){ return d[1]; })
+        ;
+    });
+    onChangeCondition();
+}
+
+function getSelectboxValue(node){
+    return node.options[node.selectedIndex].value;
+}
+
+function onChangeCondition(){
+    condition = {};
+    [
+        ['x', '#x-operator', '#x-value1', '#x-value2'],
+        ['y', '#y-operator', '#y-value1', '#y-value2'],
+        ['r', '#r-operator', '#r-value1', '#r-value2'],
+    ]
+    .forEach(function(ar){
+        if( getSelectboxValue(slide.select(ar[1]).node()) === '' ){
+            slide.select(ar[3])
+                .attr('disabled', '');
+        }
+        else {
+            slide.select(ar[3])
+                .attr('disabled', null);
+        }
+        condition[ar[0]] = {
+            value1: getSelectboxValue(slide.select(ar[2]).node()),
+            operator: getSelectboxValue(slide.select(ar[1]).node()),
+            value2: getSelectboxValue(slide.select(ar[3]).node()),
+        };
+    });
+}
+
+function drawGraph() {
+
+
 }
 
 function initGesture(){
-
-
 }
